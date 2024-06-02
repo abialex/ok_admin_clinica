e0ae60501f2c312eeaf6b1de59dce7a55bb768e1
alexis_13 <alexis.yalle.27@unsch.edu.pe>
2024-06-02 16:58:50 -0500

    fix whitenoise

--- back_hcl/settings.py
+++ back_hcl/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "back_hcl.middleware.back_hcl_handler.Log500ErrorsMiddleware",
     "django.middleware.locale.LocaleMiddleware",
-    #'whitenoise.middleware.WhiteNoiseMiddleware'
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "back_hcl.urls"
@@ -134,7 +134,7 @@ LANGUAGE_CODE = "es-ES"
 
 TIME_ZONE = "America/Lima"
 
-USE_I18N = False
+USE_I18N = True
 
 USE_TZ = False
 
